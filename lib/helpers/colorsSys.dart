5686ebb017a334439cf4d882a6cf44b5d35b8e5c
ToaMataNui1 <srivem8707@gmail.com>
2025-01-19 17:05:41 -0500

    1/19 - began work on the entry page, added strings.dart, colorsSys.dart, and started working on main.dart

---
+++ lib/helpers/colorsSys.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorSys {
+  static Color primary = Color.fromRGBO(52, 43, 37, 1);
+  static Color gray = Color.fromRGBO(137, 137, 137, 1)
+  static Color secondary = Color.fromRGBO(213, 198, 255, 1);
+
+}
